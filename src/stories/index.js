303f96a162e735f4c61a46a199f124e75d0e5808
preethi <selvampreethi360@gmail.com>
2024-05-30 15:31:46 +0530

    add config to publish as npm package by webpack bundler

---
+++ src/stories/index.js
@@ -0,0 +1,35 @@
+import Avatar from "./Components/Avatar";
+import Badge from "./Components/Badge";
+import Breadcrumb from "./Components/Breadcrumb";
+import Button from "./Components/Button";
+import Calendar from "./Components/Calendar";
+import Card from "./Components/Card";
+import Carousel from "./Components/Carousel";
+import Collapse from "./Components/Collapse";
+import Drawer from "./Components/Drawer";
+import Dropdown from "./Components/Dropdown";
+import Image from "./Components/Image";
+import Pagination from "./Components/Pagination";
+import Skeleton from "./Components/Skeleton";
+import Rate from "./Components/Rate";
+import Tag from "./Components/Tag";
+import Tooltip from "./Components/Tooltip";
+
+export {
+  Avatar,
+  Badge,
+  Breadcrumb,
+  Button,
+  Calendar,
+  Card,
+  Carousel,
+  Collapse,
+  Drawer,
+  Dropdown,
+  Image,
+  Pagination,
+  Skeleton,
+  Rate,
+  Tooltip,
+  Tag,
+};
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/stories/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: "url-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+};
