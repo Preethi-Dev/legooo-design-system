c1e3e093f2545790817a7dc84db9c15ae959b392
preethi <selvampreethi360@gmail.com>
2024-06-01 17:28:59 +0530

    add legooo logo and change storybook theme

---
+++ .storybook/myTheme.js
@@ -0,0 +1,15 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  brandTitle: "Legooo - The Design System",
+  brandImage: "/legooo.png",
+  brandTarget: "_self",
+
+  colorPrimary: "#5E6600",
+  colorSecondary: "#BBCC00",
+
+  // Toolbar default and active colors
+  barTextColor: "#9E9E9E",
+  barSelectedColor: "#BBCC00",
+  barHoverColor: "#BBCC00",
+});
